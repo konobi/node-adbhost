cf43f45a4c643382f00e90ad848eeb8bbdd3f2c6
Scott McWhirter <scott.mcwhirter@joyent.com>
2014-07-31 21:17:53 -0700

    lots of work towards getting usb working...

--- examples/shell.js
+++ examples/shell.js
@@ -1,6 +1,7 @@
-var adbhost = require('../index.js')
-var adb   = adbhost.createConnection({host: process.argv[2]});
-var shell = adb.createStream('shell:');
-process.stdin.pipe(shell);
+var adbhost = require('../index.js');
+var adb   = adbhost.createConnection({usb: true}, function(adb){
+  var shell = adb.createStream('shell:');
+  process.stdin.pipe(shell);
+  shell.pipe(process.stdout);
+});
 //process.stdin.setRawMode(true);
-shell.pipe(process.stdout);
